cec3d87abb378664bd3423218cc0a503fae067ab
NGOIE ILUNGA Isaac <160027313+irisaacilunga01@users.noreply.github.com>
2024-07-30 15:38:04 +0200

    Update page.tsx

--- app/dashboard/client/page.tsx
+++ app/dashboard/client/page.tsx
@@ -5,7 +5,7 @@ import { DataTable } from "./dataTable";
 import { ModeToggle } from "@/components/toggletheme";
 
 export default async function Page() {
-  const { rows } = await sql<Client>`SELECT * from client`;
+  const { rows } = await sql<Client>`SELECT * from personnel`;
   return (
     <div className="flex flex-col">
       <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 justify-between">
